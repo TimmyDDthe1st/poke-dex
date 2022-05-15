39c080f5eea1a07df462387f7b332a0eeb790281
Tim <tim.douglas@arcticshores.com>
2022-05-15 09:00:16 +0100

    changed layout

--- src/components/PokemonList/Card.js
+++ src/components/PokemonList/Card.js
@@ -16,25 +16,29 @@ export default function PokemonCard({
         py={3}
         px={3}
       >
-        <Typography align="left">{pokemonName}</Typography>
         {
         isLoading
           ? <Skeleton variant="circular" width={64} height={64} />
           : (
-            <Box
-              component="img"
-              sx={{
-                height: 128,
-                width: 128,
-                maxHeight: { xs: 233, md: 167 },
-                maxWidth: { xs: 350, md: 250 },
-              }}
-              alt={pokemonName}
-              src={sprite}
-            />
+            <>
+              <Typography align="left">{pokemonName}</Typography>
+
+              <Box
+                component="img"
+                sx={{
+                  height: 128,
+                  width: 128,
+                  maxHeight: { xs: 233, md: 167 },
+                  maxWidth: { xs: 350, md: 250 },
+                }}
+                alt={pokemonName}
+                src={sprite}
+              />
+              <Button onClick={handleClick}>VIEW POKEMON</Button>
+            </>
+
           )
       }
-        <Button onClick={handleClick}>VIEW POKEMON</Button>
       </Box>
     </Card>
 
