85e182b0da7c53f20d824a23c1c2a4fc3fb2185c
Tim <tim.douglas@arcticshores.com>
2022-06-06 22:16:02 +0100

    using HashRouter instead of BrowserRouter for github pages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { ThemeProvider } from '@mui/material';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './components/App';
 import theme from './theme';
 
@@ -9,9 +9,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ThemeProvider theme={theme}>
     <React.StrictMode>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </React.StrictMode>
   </ThemeProvider>,
 
